f28fd298317098a52bd40497a96740071626ea97
Mostafa <mostafamabdallah94@gmail.com>
2023-08-21 12:00:13 +0300

    last changes

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -4,23 +4,50 @@ import React from "react";
 import cer1 from "../../public/certifications/2.png";
 import cer2 from "../../public/certifications/3.png";
 import cer3 from "../../public/certifications/4.png";
+import cer4 from "../../public/certifications/5.png";
+import Gadta from "../../public/icons/Gadta.png";
+import GMP from "../../public/icons/GMP.png";
+import ISO from "../../public/icons/ISO.png";
+import logo from "../../public/Logo.png";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
     <div className="flex flex-col items-center justify-between bg-white">
-      <div className={` bg-cover flex mt-20`}>
+      <div className={`bg-cover flex flex-col mt-20`}>
+        <div className="container  px-5  xl:px-20  flex  justify-center  flex-wrap">
+          <h2 className="lg:text-7xl w-full text-3xl font-ITCAVANTGARDESTDBold font-extrabold text-primary text-center">
+            CERTIFICATES
+          </h2>
+          <div className="flex mt-5">
+            <img className="w-full md:w-3/12 px-2" src={cer4.src} alt="" />
+            <img className="w-full md:w-3/12 px-2" src={cer1.src} alt="" />
+            <img className="w-full md:w-3/12 px-2" src={cer2.src} alt="" />
+            <img className="w-full md:w-3/12 px-2" src={cer3.src} alt="" />
+          </div>
+        </div>
 
-        <div className="container  px-5  xl:px-20  flex  justify-center  gap-6 flex-wrap">
-        <h2 className="lg:text-7xl w-full  text-3xl font-ITCAVANTGARDESTDBold font-extrabold text-primary text-center">
-          CERTIFICATES
-        </h2>
-          <img src={cer1.src} alt="" />
-          <img src={cer2.src} alt="" />
-          <img src={cer3.src} alt="" />
+        <div className=" bg-gray-100 mt-5 md:flex flex-row hidden xl:px-20 items-center justify-between py-5  ">
+          <div className="logo md:w-[150px]">
+            <img alt="argemax" className="w-full" src={logo.src}></img>
+          </div>
+          <div className="flex md:w-[250px] z-50 gap-5">
+            <ul className="w-full flex gap-2 justify-end items-center">
+              <li className=" text-primary font-bold font-ITCAVANTGARDESTD tracking-[0.2rem] hover:text-secondary text-sm">
+                <img className="w-full " src={Gadta.src} alt="" />
+              </li>
+              <li className=" relative text-primary font-bold font-ITCAVANTGARDESTD tracking-[0.2rem] hover:text-secondary text-sm">
+                <img className="w-full " src={GMP.src} alt="" />
+              </li>
+              <li className=" text-primary font-bold font-ITCAVANTGARDESTD tracking-[0.2rem] hover:text-secondary text-sm">
+                <img className="w-full " src={ISO.src} alt="" />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
       <div className="w-full flex flex-col py-10">
         <div className="container py-10 px-5  xl:px-20">
           <p className="text-gray-600  text-center bottom-10 tracking-widest  text-lg font-ITCAVANTGARDESTD ">
--- components/MainBanner.tsx
+++ components/MainBanner.tsx
@@ -1,6 +1,5 @@
+"use client"
 import React from "react";
-import background from "../public/Home/Banner.jpg";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import SwiperCore, { Autoplay } from "swiper";
 type Props = {};
@@ -20,52 +19,9 @@ const MainBanner = (props: Props) => {
   return (
     <>
       <div className="w-full">
-        <Swiper {...swiperParams} slidesPerView={1} className="mySwiper">
-          <SwiperSlide>
-            <div
-              className={`h-[100vh] w-full  bg-[url('../public/Home/Banner.jpg')] bg-cover`}
-            >
-              <div className="container h-full px-5  xl:px-20  flex flex-col justify-center ">
-                <h1 className="font- lg:text-6xl text-4xl text-white ">
-                  <span className="lg:text-6xl text-4xl font-extrabold ">
-                    We Grow Through{" "}
-                  </span>
-                  <br />
-                  <span className="font-ITCAVANTGARDESTDThin lg:text-6xl text-4xl font-light">
-                    Creativity, Invention
-                    <br />
-                    And Innovation
-                  </span>
-                </h1>
-                <p className="text-white absolute bottom-10 tracking-[2rem] text-lg font-ITCAVANTGARDESTDThin hidden lg:block">
-                  QUALITY MATTERS
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              className={`h-[100vh] w-full  bg-[url('../public/sbpp/Banner.jpg')] bg-cover`}
-            >
-              <div className="container h-full px-5  xl:px-20  flex flex-col justify-center ">
-                <h1 className="font- lg:text-6xl text-4xl text-white ">
-                  <span className="lg:text-6xl text-4xl font-extrabold ">
-                    SBPP{" "}
-                  </span>
-                  <br />
-                  <span className="font-ITCAVANTGARDESTDThin lg:text-6xl text-4xl font-light">
-                    Most Important
-                    <br />
-                    And Innovation
-                  </span>
-                </h1>
-                <p className="text-white absolute bottom-10 tracking-[2rem] text-lg font-ITCAVANTGARDESTDThin hidden lg:block">
-                  QUALITY MATTERS
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+      <video  autoPlay muted loop style={{ width: '100%' }}>
+        <source src="/video.mp4" type="video/mp4" />
+      </video>
       </div>
     </>
   );
